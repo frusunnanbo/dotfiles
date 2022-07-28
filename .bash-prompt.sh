742475cc948ec0a18ba7d0654bcf4ac78914830a
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2022-07-28 20:16:41 +0200

    Removes an unnecessary space from prompt

--- .bash-prompt.sh
+++ .bash-prompt.sh
@@ -9,8 +9,7 @@ else
 fi
 
 if [ "$color_prompt" = yes ]; then
-#    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] $(__git_ps1)\$ '
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \[\033[00;93m\]$(__git_ps1)\[\033[00m\]\$ '
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00;93m\]$(__git_ps1)\[\033[00m\]\$ '
 else
     PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w $(__git_ps1)\$ '
 fi
